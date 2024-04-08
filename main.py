6603cfc116858ab4cde80be9f972e60fbd292a0c
zVitorSantos <vitorsantos4736@gmail.com>
2024-04-07 23:27:14 -0300

    feat: completo, somente fazer api agora

--- main.py
+++ main.py
@@ -1,71 +1,101 @@
 import pandas as pd
 import json
-import re
+from collections import Counter
 
 # Ler o arquivo .xlsx
-df = pd.read_excel(r'C:\Users\Vitor\Desktop\Programas\Catálogo JSON\Catálogo.xlsx')
+df = pd.read_excel(r'Catálogo.xlsx')
 
-# Extrair as colunas desejadas
+# Extrair as colunas desejadas e renomear
 df = df[["REF.:", "DESCRIÇÃO DO PRODUTO", "MEDIDAS MM HxL", "MATERIAL", "PESO", "M.", "P.", "D."]]
-
-# Renomear as colunas
 df.columns = ["ref", "descricao", "medidas", "material", "peso", "matriz", "piloto", "desenho"]
 
 # Converter as medidas para altura e largura
 df["altura"] = df["medidas"].str.split("x").str[0]
 df["largura"] = df["medidas"].str.split("x").str[1]
 
-# Selecionar apenas as colunas desejadas
-df = df[["ref", "descricao", "altura", "largura", "material", "peso", "matriz", "piloto", "desenho"]]
+# Converter os valores da coluna 'ref' para inteiros
+df['ref'] = pd.to_numeric(df['ref'], errors='coerce')  # 'coerce' para lidar com strings não numéricas
 
-# Adicionar novas colunas
-df.insert(df.columns.get_loc("peso") + 1, "custo", "")
-df.insert(df.columns.get_loc("descricao") + 1, "categoria_1", "")
-df.insert(df.columns.get_loc("categoria_1") + 1, "categoria_2", "")
-df.insert(df.columns.get_loc("categoria_2") + 1, "categoria_3", "")
+# Filtrar o DataFrame para incluir apenas produtos com referência menor ou igual a 5250
+df_filtrado = df[df['ref'] <= 5250].copy()  # Fazer uma cópia para evitar alterações no DataFrame original
 
-# Lista de categorias para buscar na descrição
-categorias = ['bridão', 'âmago', 'passador', 'fivela', 'ornavi', 'pingente', 'laço', 'salomé', 'animais', 'coração']
+# Listas de palavras para cada prioridade
+prioridade_1 = ['bioluz', 'brilha', 'brilha natal', 'natal', 'sousplat', 'tdecorare']
+prioridade_2 = ['bridão', 'âmago', 'passador', 'fivela', 'ornavi', 'pingente', 'salomé', 'animais', 'tira']
+prioridade_3 = ['laço', 'flor', 'coração', 'botão', 'mandala', 'pedra']
+complementos = ['resina', 'strass']
+modelos = ['i', 't', 'y', 'v']
 
-# Função para buscar categorias na descrição
-def buscar_categorias(descricao):
-    if isinstance(descricao, str):  # Verificar se a descrição é uma string
-        descricao = descricao.lower()  # Converter a descrição para minúsculas
-        # Tratar 'Bioluz', 'brilha', 'brilha natal' e 'natal' de maneira especial
-        if any(word in descricao for word in ['bioluz', 'brilha', 'brilha natal', 'natal']):
-            return ['Bioluz' if 'bioluz' in descricao else 'Brilha Natal', '', '']
+def buscar_palavras(descricao):
+    categorias = pd.Series({
+        "categoria_1": "",
+        "categoria_2": "",
+        "categoria_3": "",
+        "complementos": ""
+    })
+    
+    if isinstance(descricao, str):
+        descricao = descricao.lower()
         
-        # Para todas as outras categorias, preenchê-las na ordem em que aparecem na descrição
-        categorias_encontradas = [categoria for categoria in categorias if categoria in descricao][:3]  # Limitar a 3 categorias
+        # Verificar complementos na descrição
+        complementos_encontrados = [complemento for complemento in complementos if complemento in descricao]
+        if complementos_encontrados:
+            categorias['complementos'] = ', '.join(complementos_encontrados).capitalize()
         
-        # Extrair o formato após "Tira em "
-        match = re.search(r'tira em (\w+)', descricao)
-        if match and 'Tira' not in categorias_encontradas:  
-            categorias_encontradas = categorias_encontradas[:2] 
-            categorias_encontradas.append('Tira')
-            categorias_encontradas.append(match.group(1))
+        # Verificar palavras de prioridade 1
+        for palavra in prioridade_1:
+            if palavra in descricao:
+                categorias['categoria_1'] = palavra.capitalize()
+                break  # Parar no primeiro match
         
-        return categorias_encontradas + [''] * (3 - len(categorias_encontradas)) 
-    else:
-        return ['', '', '']
+        # Verificar palavras de prioridade 2
+        for palavra in prioridade_2:
+            if palavra in descricao:
+                if 'tira em' in descricao:
+                    for modelo in modelos:
+                        if f'tira em {modelo}' in descricao:
+                            categorias['categoria_2'] = f'Tira em {modelo.capitalize()}'
+                            return categorias
+                else:
+                    categorias['categoria_2'] = palavra.capitalize()
+                    break  # Parar no primeiro match
+        
+        # Verificar palavras de prioridade 3
+        prioridade_3_encontradas = [palavra for palavra in prioridade_3 if palavra in descricao]
+        if prioridade_3_encontradas:
+            if len(prioridade_3_encontradas) == 1:
+                categorias['categoria_2'] = prioridade_3_encontradas[0].capitalize()
+            else:
+                categorias['categoria_2'] = prioridade_3_encontradas[0].capitalize()
+                categorias['categoria_3'] = ', '.join(prioridade_3_encontradas[1:]).capitalize()
+    
+    return categorias
 
-# Aplicar a função para buscar categorias
-df[['categoria_1', 'categoria_2', 'categoria_3']] = pd.DataFrame(df['descricao'].apply(buscar_categorias).tolist(), index=df.index)
+# Aplicar a função 'buscar_palavras' e armazenar o resultado nas colunas correspondentes
+categorias = df_filtrado['descricao'].apply(buscar_palavras)
+df_filtrado[['categoria_1', 'categoria_2', 'categoria_3', 'complementos']] = categorias
 
-# Aplicar a função para buscar categorias
-df[['categoria_1', 'categoria_2', 'categoria_3']] = pd.DataFrame(df['descricao'].apply(buscar_categorias).tolist(), index=df.index)
 # Substituir valores NaN por uma string vazia
-df = df.fillna('')
+df_filtrado = df_filtrado.fillna('')
 
 # Remover espaços no início e no fim das strings
-df = df.apply(lambda x: x.map(lambda y: y.strip() if isinstance(y, str) else y))
+df_filtrado = df_filtrado.apply(lambda x: x.map(lambda y: y.strip() if isinstance(y, str) else y))
 
 # Considerar qualquer valor não vazio como True
-df[['matriz', 'piloto', 'desenho']] = df[['matriz', 'piloto', 'desenho']].apply(lambda x: x.map(lambda y: False if y == '' else True))
+df_filtrado[['matriz', 'piloto', 'desenho']] = df_filtrado[['matriz', 'piloto', 'desenho']].apply(lambda x: x.map(lambda y: False if y == '' else True))
 
-# Converter o DataFrame para um dicionário
-data = {"produtos": df.to_dict(orient="records")}
+# Adicionar a coluna 'valor' ao DataFrame
+df_filtrado['valor'] = ""
+
+# Converter os valores da coluna 'ref' para inteiros, ignorando os valores NaN
+df_filtrado['ref'] = df_filtrado['ref'].apply(lambda x: int(x) if pd.notnull(x) else x)
+
+# Converter o DataFrame filtrado para uma lista de dicionários mantendo a ordem das colunas
+data = df_filtrado[["ref", "descricao", "categoria_1", "categoria_2", "categoria_3", "complementos", "altura", "largura", "material", "peso", "valor", "matriz", "piloto", "desenho"]].to_dict(orient="records")
+
+# Criar o dicionário final para salvar como JSON
+data_final = {"produtos": data}
 
 # Salvar o dicionário como um arquivo JSON
-with open(r'C:\Users\Vitor\Desktop\Programas\Catálogo JSON\Catalogo.json', 'w', encoding='utf-8') as file:
-    json.dump(data, file, ensure_ascii=False, indent=4)  # ensure_ascii=False para manter os caracteres especiais
+with open('Catalogo.json', 'w', encoding='utf-8') as file:
+    json.dump(data_final, file, ensure_ascii=False, indent=4)
